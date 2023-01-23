70be327b0bfb71b0ebab7c51ac02973c503f2b4c
David Daza <drdaza18@gmail.com>
2023-01-23 14:07:48 +0100

    added player

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,5 +1,20 @@
 package com.factoriaf5.rps.application;
 
+import java.util.ArrayList;
+
 public class Game {
+    
+    private ArrayList<Player> Players;
+
+    public Game(){
+        Players = new ArrayList<>(2);
+    }
+
+    public void setPlayer(Player player){
+        Players.add(player);
+    }
+    public ArrayList<Player> getPlayer(){
+        return Players;
+    }
     
 }
